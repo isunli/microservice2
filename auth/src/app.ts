b6cbde3ff25d5eb7754bc0d0fc100a136bea5f8c
lisun <li@rasp.com>
2020-10-12 03:02:51 +0000

    add test

---
+++ auth/src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import coockieSession from "cookie-session";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  coockieSession({
+    signed: false, //not encrypt the jwt
+    secure: true,
+  })
+);
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,31 +1,5 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
 import mongoose from "mongoose";
-import coockieSession from "cookie-session";
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-const app = express();
-app.set("trust proxy", true);
-app.use(json());
-app.use(
-  coockieSession({
-    signed: false, //not encrypt the jwt
-    secure: true,
-  })
-);
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-app.all("*", async () => {
-  throw new NotFoundError();
-});
-app.use(errorHandler);
+import { app } from "./app";
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY not defined");
