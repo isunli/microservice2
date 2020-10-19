2c7c8d6638113f5553a6ca4591b06178e4c91003
lisun <li@rasp.com>
2020-10-19 03:33:55 +0000

    finish tickets route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,8 @@ import coockieSession from "cookie-session";
 import { createTicketRouter } from "./routes/new";
 import { errorHandler, NotFoundError, currentUser } from "@sltickets/common";
 import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true);
@@ -18,6 +20,8 @@ app.use(
 app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 app.all("*", async () => {
   throw new NotFoundError();
 });
