f6ca4ffa13d43874ce875ecac9ec43bda4aa550e
LS <ls@example.com>
2020-11-16 11:36:52 -0500

    add common

---
+++ payments/src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import coockieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@sltickets/common";
+import { createChargeRouter } from "./routes/new";
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  coockieSession({
+    signed: false, //not encrypt the jwt
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+app.use(currentUser);
+app.use(createChargeRouter);
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
